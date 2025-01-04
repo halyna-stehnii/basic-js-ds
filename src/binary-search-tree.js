3a509d18a7dad2420534506c912952fb0651391d
Halyna Stehnii <galyna.stegniy@gmail.com>
2025-01-04 17:37:13 +0100

    feat: binary-search-tree add and addNode added

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -15,9 +15,29 @@ class BinarySearchTree {
     return this.bstRoot;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const node = new Node(data);
+    if (this.bstRoot === null) {
+      this.bstRoot = node;
+    } else {
+      this.addNode(this.bstRoot, node);
+    }
+  }
+
+  addNode(bstRoot, node) {
+    if (node.data < this.bstRoot.data) {
+      if (bstRoot.left === null) {
+        bstRoot.left = node;
+      } else {
+        this.addNode(bstRoot.left, node);
+      }
+    } else {
+      if (bstRoot.right === null) {
+        bstRoot.right = node;
+      } else {
+        this.addNode(bstRoot.right, node);
+      }
+    }
   }
 
   has(/* data */) {
